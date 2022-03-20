9db0ed80a7578b35516c7ae201eff65957627501
sreenathreddy961 <sreenathreddy961@gmail.com>
2022-03-20 22:51:47 +0530

    Book appointment, headerbar UI bugs fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import '../styles/Home.css';
-import '../styles/2.818011ed.chunk.css';
+import '../styles/Bootstrap.css';
 import Homebg from '../assets/Homebg.jpg';
+import {Link} from 'react-router-dom';
 
 export default function Home() {
     return (
@@ -10,18 +11,18 @@ export default function Home() {
                 We have an offer for the first 100 slots, Hurry up & book your appointment
             </marquee>
             <div className="app_container">
-                <div class="container d-flex flex-wrap">
-                    <div class="wrapper_card">
-                        <div class="card text-center p-2" style={{ backgroundColor: "transparent", border: "none" }}>
-                            <div class="card-header">
-                                <h1 class="display-1">Good Food is the Soul for Healthy life</h1>
+                <div className="container d-flex flex-wrap">
+                    <div className="wrapper_card">
+                        <div className="card text-center p-2" style={{ backgroundColor: "transparent", border: "none" }}>
+                            <div className="card-header">
+                                <h1 className="display-1">Good Food is the Soul for Healthy life</h1>
                             </div>
-                            <div class="card-body pd-3">
+                            <div className="card-body pd-3">
                                 <h2>Just because you are not sick doesn't mean you're healthy</h2>
-                                <p>Book Appoinment, Insure Good Health -<cite>Foodie Family</cite></p>
+                                <p style={{ textAlign:"center"}}>Book Appoinment, Insure Good Health -<cite>Foodie Family</cite></p>
                             </div>
                             <div>
-                                <a href="/Contactus" class="btn btn-success btn-lg">Book Appoinment</a>
+                                <Link to="/Contactus" className="btn btn-success btn-lg">Book Appoinment</Link>
                             </div>
                             <div></div>
                         </div>
